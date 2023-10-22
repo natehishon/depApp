59e25acc52b02edbfe4574bfd6cbe7b01a4919e8
nate hishon <nathan.hishon@gmail.com>
2023-10-22 14:58:12 -0400

    vue set up

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+import { createApp } from "vue";
+import Home from './views/Home.vue'
+const app = createApp({
+    components: {
+        Home
+    }
+});
+
+app.mount("#app");
---
+++ resources/js/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        hey
+        <Test />
+    </div>
+</template>
+
+
+<script setup>
+import Test from "../components/Test.vue";
+
+</script>
